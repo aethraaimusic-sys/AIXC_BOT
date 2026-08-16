4f5e1f187a1879e9b0aaec2d3d4e65c574040b33
aethraaimusic-sys <aethraaimusic@gmail.com>
2026-08-16 17:18:52 +0100

    Remove investment plans and add short bot name
    Removed INVESTMENT_PLANS dictionary and added BOT_SHORT_NAME.

--- config.py
+++ config.py
@@ -1,9 +1,3 @@
 BOT_NAME = "Austin Investment X Course"
 
-INVESTMENT_PLANS = {
-    "Starter": 200,
-    "Silver": 500,
-    "Gold": 800,
-    "Platinum": 1000,
-    "VIP": 1500
-}
+BOT_SHORT_NAME = "AIXC"
