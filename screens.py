9867cfd062f87812564811e6107ed7cf59a584bd
aethraaimusic-sys <aethraaimusic@gmail.com>
2026-08-16 17:15:13 +0100

    Add home_screen function for user interface

---
+++ screens.py
@@ -0,0 +1,19 @@
+from config import BOT_NAME
+
+
+def home_screen(user):
+
+    return f"""
+
+🤖 {BOT_NAME}
+
+Welcome, {user}
+
+💰 Balance: $0.00
+📈 Active Investment: $0.00
+💵 Earnings: $0.00
+👥 Referrals: 0
+
+Choose an option below.
+
+"""
